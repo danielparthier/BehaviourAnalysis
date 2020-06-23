552ab81b7b58423048f766f7760eddcb6b6f1d39
danielparthier <daniel.parthier@gmail.com>
2020-06-23 18:13:46 +0200

    add reference input which allows batch analysis

--- R/SpeedDistCumDist.R
+++ R/SpeedDistCumDist.R
@@ -4,19 +4,31 @@
 #' @param CoordTable A table including coordinates of labels.
 #' @param SpeedRef A string indicating the label used for computing speed.
 #' @param Interval A double corresponding to sampling rate.
-#'
+#' @param ReferenceColumn A string indicating the reference column.
+#' @param Overwrite A bool indicating if output should be overwritten if it exists already (default = TRUE).
+#' 
 #' @return Modifies existing DataTable.
 #' @export
 DistSpeedCalc <- function(CoordTable,
                           SpeedRef,
-                          Interval) {
-  SpeedInstDistRef <- VariableNameCheck(DataTable = CoordTable, NameString = paste0("InstDistance", SpeedRef))
-  SpeedCumDistRef <- VariableNameCheck(DataTable = CoordTable, NameString = paste0("CumDist", SpeedRef))
-  SpeedSpeedRef <- VariableNameCheck(DataTable = CoordTable, NameString = paste0("Speed", SpeedRef))
-  
+                          Interval,
+                          ReferenceColumn = "",
+                          Overwrite = TRUE) {
+  if(Overwrite) {
+    SpeedInstDistRef <- paste0("InstDistance", SpeedRef)
+    SpeedCumDistRef <- paste0("CumDist", SpeedRef)
+    SpeedSpeedRef <- paste0("Speed", SpeedRef)
+  } else {
+    SpeedInstDistRef <- VariableNameCheck(DataTable = CoordTable, NameString = paste0("InstDistance", SpeedRef))
+    SpeedCumDistRef <- VariableNameCheck(DataTable = CoordTable, NameString = paste0("CumDist", SpeedRef))
+    SpeedSpeedRef <- VariableNameCheck(DataTable = CoordTable, NameString = paste0("Speed", SpeedRef))
+  }
+
   CoordTable[, eval(SpeedInstDistRef) := sqrt((data.table::shift(get(paste0(SpeedRef, "_x")), type = "lead") - get(paste0(SpeedRef, "_x")))^2 +
                                                 (data.table::shift(get(paste0(SpeedRef, "_y")),
-                                                                   type = "lead") - get(paste0(SpeedRef, "_y")))^2)][
-                                                                     , eval(SpeedCumDistRef) := cumsum(get(SpeedInstDistRef))][
+                                                                   type = "lead") - get(paste0(SpeedRef, "_y")))^2), by = ReferenceColumn]
+  CoordTable[
+                                                                     , eval(SpeedCumDistRef) := cumsum(get(SpeedInstDistRef)), by=ReferenceColumn]
+  CoordTable[
                                                                        , eval(SpeedSpeedRef) := get(SpeedInstDistRef)/Interval]
 }
